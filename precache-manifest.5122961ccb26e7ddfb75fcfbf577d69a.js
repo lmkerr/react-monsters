6aa04dd9491e09a17da8f82b564ee36d1fc4fc7d
Loren M. Kerr <loren@lorenkerr.com>
2019-11-12 20:05:43 -0700

    Updates

---
+++ precache-manifest.5122961ccb26e7ddfb75fcfbf577d69a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bf1971869939e1e47680b25d323d6e",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "2e711a8ba22d61dd885c",
+    "url": "/react-monsters/static/css/main.6a9963d4.chunk.css"
+  },
+  {
+    "revision": "006a2fd3e952ada59457",
+    "url": "/react-monsters/static/js/2.cf0ec2a7.chunk.js"
+  },
+  {
+    "revision": "2e711a8ba22d61dd885c",
+    "url": "/react-monsters/static/js/main.6a551c15.chunk.js"
+  },
+  {
+    "revision": "5d54ed567b57158449c2",
+    "url": "/react-monsters/static/js/runtime-main.30d8e865.js"
+  }
+]);
