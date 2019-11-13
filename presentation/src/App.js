80bc7de50b488499f0142f2ef679516c12d158d4
Loren M. Kerr <loren@lorenkerr.com>
2019-11-12 17:19:50 -0700

    Added search field input and added a quick filter to the main app.

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,31 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 /* Internal */
-import { CardList } from './component/card-list/card-list.component';
+import { CardList } from "./component/card-list/card-list.component";
 
-class App extends Component { 
+class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
-  }
-  
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({monsters: users}));
+      monsters: [],
+      searchField: ""
+    };
   }
 
-  render() { 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type="search"
+          placeholder="search monsters..."
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
-   )
-  }  
+    );
+  }
 }
 
 export default App;
